d339695a8a9c79c62245917c0fc8111248b44ad4
aayanqazi <aayanqazi@yahoo.com>
2018-10-05 15:35:18 +0500

    Integrated post css

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path')
 
 module.exports = {
-    context: path.resolve(__dirname),
-    entry: './app/app.tsx',
-    output: {
-        path: __dirname + '/public',
-        filename: 'build/app.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' }
-        ]
-    }
+  context: path.resolve(__dirname),
+  entry: './app/app.tsx',
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    require('autoprefixer')
+  ]
 }
