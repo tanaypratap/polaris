ff49042fe9549d130f76b6025a319e3b4eca2f62
Shane Mckenna <shanemckennadev@gmail.com>
2018-10-14 19:09:29 -0700

    Restructure

--- client/app/components/TestComponent.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export const TestComponent = () => (
-    <div> This is just a TestComponent </div>
-)
--- client/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./home";
-export * from "./about";
-export * from "./header";
-export * from "./error-page";
